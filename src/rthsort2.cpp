5cd91a47155e50b39ace6602db868f78b7873f39
Stephen Mathews <busysteve@gmail.com>
2018-08-15 20:06:25 -0400

    Got the external sort working with sort of powers of 2

--- src/rthsort2.cpp
+++ src/rthsort2.cpp
@@ -21,6 +21,7 @@ extern "C" SEXP rthsort2_double(SEXP a, SEXP decreasing, SEXP inplace,
   
   thrust::device_vector<double> dx;
   thrust::host_vector<double> hm;
+  //thrust::host_vector<double> hx(len);
   //double *hm = new double[len];  // Host memory
   //std::unique_ptr<double[]> hm{new double[len]};
   
@@ -31,8 +32,9 @@ extern "C" SEXP rthsort2_double(SEXP a, SEXP decreasing, SEXP inplace,
   
   std::vector<int> chunks;
   
+  int split = 4;
   
-  int parts = len / 4;
+  int parts = len / split;
   
   
   //for( int i=0; i<len; i+=10000000 )
@@ -49,7 +51,7 @@ extern "C" SEXP rthsort2_double(SEXP a, SEXP decreasing, SEXP inplace,
   for( int chunk=0; chunk < chunks.size()-1; chunk++ )
   {
     log_num3( chunk, chunks[chunk], chunks[chunk+1] );
-    
+    dx.clear();
     dx.assign( hm.begin() + chunks[chunk], hm.begin() + chunks[chunk+1] );
     
     // sort, then copy back to R vector
@@ -62,19 +64,96 @@ extern "C" SEXP rthsort2_double(SEXP a, SEXP decreasing, SEXP inplace,
       thrust::sort(  thrust::device, dx.begin(), dx.end()  );
     }
     
-    thrust::copy( dx.begin(), dx.begin()+( chunks[chunk+1]-chunks[chunk] ), (double*)(&(hm[ chunks[chunk] ])) );
+    //thrust::copy( dx.begin(), dx.begin()+( chunks[chunk+1]-chunks[chunk] ), (double*)(&(hm[ chunks[chunk] ])) );
+    //thrust::copy( dx.begin(), dx.begin()+( chunks[chunk+1]-chunks[chunk] ), hm.begin()+ chunks[chunk] );
+    thrust::copy( dx.begin(), dx.end(), hm.begin()+ chunks[chunk] );
   }
-  
 
   log_sz( "Entering split sort loop");
   
+  
+  
+  //for( int i=1; i < split; i++ )
+  //for( int j=i; j < split; j++ )
+  {
+    int chunk = (len / split);
+    int slice = (chunk / split);
+    //log_num3( i, j, (len / split) );
+    //thrust::swap_ranges( hm.begin()+slice*j+slice*i,  hm.begin()+slice*j+slice*i+slice, hm.begin()+slice*j*i );
+    thrust::swap_ranges( hm.begin()+slice*1,  hm.begin()+slice*1+slice, hm.begin()+slice*4 );
+    thrust::swap_ranges( hm.begin()+slice*2,  hm.begin()+slice*2+slice, hm.begin()+slice*8 );
+    thrust::swap_ranges( hm.begin()+slice*3,  hm.begin()+slice*3+slice, hm.begin()+slice*12 );
+    
+    thrust::swap_ranges( hm.begin()+slice*6,  hm.begin()+slice*6+slice, hm.begin()+slice*13 );
+    thrust::swap_ranges( hm.begin()+slice*7,  hm.begin()+slice*7+slice, hm.begin()+slice*9 );
+    
+    thrust::swap_ranges( hm.begin()+slice*9,  hm.begin()+slice*9+slice, hm.begin()+slice*13 );
+    thrust::swap_ranges( hm.begin()+slice*11,  hm.begin()+slice*11+slice, hm.begin()+slice*14 );
+  }
+   
+  
+  
+  log_sz( "Entering second sort loop");
+  
+  for( int chunk=0; chunk < chunks.size()-1; chunk++ )
+  {
+    log_num3( chunk, chunks[chunk], chunks[chunk+1] );
+    dx.clear();
+    dx.assign( hm.begin() + chunks[chunk], hm.begin() + chunks[chunk+1] );
+    
+    // sort, then copy back to R vector
+    if (INTEGER(decreasing)[0] )
+    {
+      thrust::sort( thrust::device, dx.begin(), dx.end(), thrust::greater<double>()  );
+    }
+    else
+    {
+      thrust::sort(  thrust::device, dx.begin(), dx.end()  );
+    }
+    
+    //thrust::copy( dx.begin(), dx.begin()+( chunks[chunk+1]-chunks[chunk] ), (double*)(&(hm[ chunks[chunk] ])) );
+    //thrust::copy( dx.begin(), dx.begin()+( chunks[chunk+1]-chunks[chunk] ), hm.begin()+ chunks[chunk] );
+    thrust::copy( dx.begin(), dx.end(), hm.begin()+ chunks[chunk] );
+  }
+  
+  
+  
+  log_sz( "Entering third and partial final sort loop");
+  //if(false)
+  for( int chunk=0; chunk < chunks.size()-2; chunk++ )
+  {
+    log_num3( chunk, chunks[chunk], chunks[chunk+1] );
+    dx.clear();
+    int chunk_size = chunks[chunk+1] - chunks[chunk];
+    dx.assign( hm.begin() + chunks[chunk]+chunk_size/2, hm.begin() + chunks[chunk+1]+chunk_size/2 );
+    
+    // sort, then copy back to R vector
+    if (INTEGER(decreasing)[0] )
+    {
+      thrust::sort( thrust::device, dx.begin(), dx.end(), thrust::greater<double>()  );
+    }
+    else
+    {
+      thrust::sort(  thrust::device, dx.begin(), dx.end()  );
+    }
+    
+    //thrust::copy( dx.begin(), dx.begin()+( chunks[chunk+1]-chunks[chunk] ), (double*)(&(hm[ chunks[chunk] ])) );
+    //thrust::copy( dx.begin(), dx.begin()+( chunks[chunk+1]-chunks[chunk] ), hm.begin()+ chunks[chunk] );
+    thrust::copy( dx.begin(), dx.end(), hm.begin()+ chunks[chunk] + chunk_size / 2 );
+  }
+  
+  
+  
+  
+  if(false)
+  //for( int shift=0; shift < chunks.size()-1; shift++ )
   for( int shift=0; shift < chunks.size()-1; shift++ )
   {
     dx.clear();
 
-    log_sz( "Entering split chunk pull");
+    log_sz( ""); log_sz( "Entering split chunk pull");
     log_num( shift );
-    log_sz( "");
+    
     
     
     
@@ -89,14 +168,16 @@ extern "C" SEXP rthsort2_double(SEXP a, SEXP decreasing, SEXP inplace,
       int slice_start = slice; // * shift;
       int slice_end = slice_start + slice;
       int offset = (((cut_end - cut_start) / (chunks.size()-1)   ) * shift  );
-      int offset_shift = (((cut_end - cut_start) / (chunks.size()-1)   ) * sweep  );
+      //int offset = (((cut_end - cut_start) / (chunks.size()-1)   ) * sweep  );
+      int offset_shift = (((cut) / (chunks.size()-1)   ) * shift  );
       int slice_shift = slice * shift;
       log_num3( (slice), (cut_offset), (offset) );
       log_num3( ( slice_start ), ( slice_end ), ( slice_shift ) );
       log_num3( (cut), ( cut_start), ( cut_end )  );
       log_num2(  ( cut_start + offset_shift ),  ( cut_start + offset_shift + slice )  );
-      //dx.insert( dx.end(), hm.begin()+( cut_start ),  hm.begin()+( cut_start + slice + cut_offset )  );
+
       dx.insert( dx.end(), hm.begin()+( cut_start + offset_shift ),  hm.begin()+( cut_start + offset_shift + slice )  );
+      //dx.insert( dx.end(), hm.begin()+( cut_start + offset_shift ),  hm.begin()+( cut_start + offset_shift + slice )  );
       log_sz_num( "dx.size()", dx.size() );
     }
     
@@ -111,23 +192,24 @@ extern "C" SEXP rthsort2_double(SEXP a, SEXP decreasing, SEXP inplace,
       thrust::sort(  thrust::device, dx.begin(), dx.end()  );
     }
     
-    log_sz( "");  log_sz( "Returning sorted chunks");
+    log_sz( "Returning sorted chunks");
 
     for( int sweep=0; sweep < chunks.size()-1; sweep++ )
     {
       log_num( sweep );
-      int cut_start = chunks[sweep];
-      int cut_end = chunks[sweep+1];
+      int cut_start = chunks[shift];
+      int cut_end = chunks[shift+1];
       int cut = (cut_end-cut_start);
       int slice = ( cut / (chunks.size() - 1 ) );
       int slice_start = slice; // * shift;
       int slice_end = slice_start + slice;
-      int offset = cut_start + (((cut_end - cut_start) / (chunks.size()-1)   ) * shift  );
+      int offset = cut_start + (((cut )/ (chunks.size()-1)   ) * sweep   );
+      //int offset = cut_start + (((cut) / (chunks.size()-1)   ) * shift  );
+      //int offset = cut_start + (((cut_end - cut_start) / (chunks.size()-1)   ) * shift  );
       int slice_shift = slice * shift;
       log_num( cut );
       log_num2( ( cut_start), ( cut_end )  );
       log_num( offset );
-
       log_num( ( slice_start ) );
       log_num( ( slice ) );
       log_num( ( slice_start ) );
@@ -135,11 +217,18 @@ extern "C" SEXP rthsort2_double(SEXP a, SEXP decreasing, SEXP inplace,
       log_num( ( slice_shift ) );
       
       log_num3(  ( slice*sweep ) , ( slice*sweep + slice )  , ( offset )  )
-      thrust::copy( 
-        dx.begin() + ( slice*sweep ), 
-        dx.begin() + ( slice*sweep + slice ), 
-        hm.begin() + ( offset )  //+ slice*shift) // chunks[sweep]// + cut * sweep
-      );
+        thrust::copy( 
+          dx.begin() + ( slice*sweep ), 
+          dx.begin() + ( slice*sweep + slice ), 
+          hm.begin() + ( offset ) 
+          //hx.begin() + ( slice*sweep+chunks[shift] ) //+ slice*shift) // chunks[sweep]// + cut * sweep
+        );
+      
+      //thrust::copy( 
+      //  dx.begin() + ( slice*sweep ), 
+      //  dx.begin() + ( slice*sweep + slice ), 
+      //  hm.begin() + ( offset )  //+ slice*shift) // chunks[sweep]// + cut * sweep
+      //);
       
     }
     
@@ -148,9 +237,17 @@ extern "C" SEXP rthsort2_double(SEXP a, SEXP decreasing, SEXP inplace,
     
   }
   
-
-  log_sz( "Sorting complete");
+  /*
+  int chunk = (len / split) / split;
   
+  for( int order=1, t=split; order < split; order+=1, t+=(split-order) )
+  {
+    log_num3( split, order, t );
+    thrust::swap_ranges( hm.begin()+chunk*(order),  hm.begin()+chunk*(order)+chunk, hm.begin()+chunk*(t) );
+  }
+  */
+  
+  log_sz( "Sorting complete");
   
   
   
@@ -170,6 +267,7 @@ extern "C" SEXP rthsort2_double(SEXP a, SEXP decreasing, SEXP inplace,
     PROTECT(b = allocVector(REALSXP, len) );
     
     thrust::copy(hm.begin() , hm.end(), (double*)REAL(b));
+    //thrust::copy(hx.begin() , hx.end(), (double*)REAL(b));
     
     UNPROTECT(1);
     log_sz( "Done");
--- test/Rth.rthsort2.compare.R
+++ test/Rth.rthsort2.compare.R
@@ -14,61 +14,62 @@ rthsorttest2 <- function( base=1, power=c(1))
     rdx <- runif( base^i )
     elaps <- system.time( stdsort <- sort( rdx ) )
     srun <- c( srun, elaps[3] ) 
-    #elaps <- system.time( rthsrt <- rthsort( rdx ) )
-    #rthrun <- c( rthrun, elaps[3] ) 
+    elaps <- system.time( rthsrt <- rthsort( rdx ) )
+    rthrun <- c( rthrun, elaps[3] ) 
     
     #elaps <- system.time( rthsrt2 <- sort( rdx ) )
     elaps <- system.time( rthsrt2 <- rthsort2( rdx ) )
     rthrun2 <- c( rthrun2, elaps[3] ) 
     
-    print ( all.equal( stdsort, rthsrt2 ) )
+    #print ( all.equal( stdsort, rthsrt2 ) )
     
-    print ( summary( stdsort ) )
-    print ( summary( rthsrt2 ) )
+    #print ( summary( stdsort ) )
+    #print ( summary( rthsrt2 ) )
     
-    print ( head( stdsort, 10 ) )
-    print ( head( rthsrt2, 10 ) )
+    #print ( head( stdsort, 10 ) )
+    #print ( head( rthsrt2, 10 ) )
     
-    print ( tail( stdsort, 10 ) )
-    print ( tail( rthsrt2, 10 ) )
+    #print ( tail( stdsort, 10 ) )
+    #print ( tail( rthsrt2, 10 ) )
 
-    dat <- matrix( c( stdsort, rthsrt2), 2, byrow = TRUE )
+    #dat <- matrix( c( stdsort, rthsrt2), 3, byrow = TRUE )
     
-    barplot( dat, 
-             main="Sort Compare", 
-             horiz=FALSE, 
-             #col=c( "black", "green", "lightblue" ), 
-             col=c( "black", "lightblue" ), 
-             beside=TRUE, 
-             names.arg = c( 1:2 ), 
-             xlab = "Time in seconds", 
-             ylab = paste( base, "to the nth power "  ), 
-             #legend.text = c( "R", "Rth", "Rth2" ) 
-             legend.text = c( "R", "Rth2" ) 
-    )
+    
+    #barplot( dat, 
+    #         main="Sort Compare", 
+    #         horiz=FALSE, 
+    #         #col=c( "black", "green", "lightblue" ), 
+    #         col=c( "black", "lightblue" ), 
+    #         beside=TRUE, 
+    #         names.arg = c( 1:2 ), 
+    #         xlab = "Time in seconds", 
+    #         ylab = paste( base, "to the nth power "  ), 
+    #         #legend.text = c( "R", "Rth", "Rth2" ) 
+    #         legend.text = c( "R", "Rth2" ) 
+    #)
     
         
   }
   
-  #df <- matrix( c( srun, rthrun, rthrun2), 3, byrow = TRUE )
-  df <- matrix( c( srun, rthrun2), 2, byrow = TRUE )
+  df <- matrix( c( srun, rthrun, rthrun2), 3, byrow = TRUE )
+  #df <- matrix( c( srun, rthrun2), 2, byrow = TRUE )
   df <- df[,-1]
 
   barplot( df, 
            main="R vs Rth(CUDA) ", 
            horiz=TRUE, 
-           #col=c( "black", "green", "lightblue" ), 
-           col=c( "black", "red" ), 
+           col=c( "black", "green", "lightblue" ), 
+           #col=c( "black", "red" ), 
            beside=TRUE, 
            names.arg = c( power ), 
            xlab = "Time in seconds", 
            ylab = paste( base, "to the nth power "  ), 
-           #legend.text = c( "R", "Rth", "Rth2" ) 
-           legend.text = c( "R", "Rth2" ) 
+           legend.text = c( "R", "Rth", "Rth2" ) 
+           #legend.text = c( "R", "Rth2" ) 
   )
 }
 
 #rthsorttest2( 2, 24 )
-rthsorttest2( 2, 8 )
+rthsorttest2( 2, 25:27 )
 
 
